90e2f7fc2952e86ffab6b4e377cf2e4ef7416a3b
Daniel Abrao <danielpaladar@gmail.com>
2020-01-30 22:59:06 -0300

    Update app files

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const swaggerRoutes = require('./routes/swagger-route');
 
 const app = express();
 
+const port = process.env.APP_PORT || process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,7 +27,6 @@ app.all('', (req, res) => {
 });
 
 // start node server
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App UI available http://localhost:${port}`);
   console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
